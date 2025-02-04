5de71b8ca3eb5ff45892b7759e23e7544f25ca0f
Unbbox <ehe110894@naver.com>
2025-02-04 13:35:59 +0900

    update js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ import "./Movie.css";
 function Movie({ id, poster_path, title, release_date }) {
   return (
     <div key={id} className="movie__info">
-      <Link to={`/movie/${id}`} className="movie__info__text">
+      <Link to={`${process.env.PUBLIC_URL}/movie/${id}`} className="movie__info__text">
         <img src={poster_path} alt={title} className="movie__img" />
         <div>
           <h2>{title.length > 30 ? `${title.slice(0, 30)}...` : title}</h2>

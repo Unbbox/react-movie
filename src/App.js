6be41a094230861e694445d14d3273228e9d2a77
Unbbox <ehe110894@naver.com>
2025-02-04 13:12:25 +0900

    update js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
